7354ead86c6aa1afdbf6a54c5599c94f4c29df1a
Kartik Chauhan <lkchauhan.725@gmail.com>
2018-05-27 17:54:13 +0530

    fixed logo
    Signed-off-by: Kartik Chauhan <lkchauhan.725@gmail.com>

--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -9,6 +9,7 @@ const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
             </p>
             <div className='center'>
                 <div className='center form pa4 br3 shadow-5'>
+                <p><strong>Paste the URL:</strong></p>
                 <input type='text' className='f4 pa2 w-70 center' onChange={onInputChange}/>
                 <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple pointer' onClick={onButtonSubmit}>Detect</button>
                 </div>
